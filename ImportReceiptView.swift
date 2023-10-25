bc4e33de2e9862cd0a400696452865292f937f62
YourUsername <tangfavish@gmail.com>
2023-10-24 18:54:09 -0700

    Initial Commit

---
+++ ImportReceiptView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct ImportReceiptView: View {
+    var body: some View {
+        VStack {
+            Text("Import Receipt View")
+                .font(.title)
+                .padding()
+            Text("This is a placeholder")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+    }
+}
